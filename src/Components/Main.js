437702362f130045fa870e69be1b8aeb9c8ef256
Nestted <63265938+Nestted@users.noreply.github.com>
2020-10-27 20:58:15 -0700

    Update verbiage

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -9,8 +9,7 @@ class Main extends Component {
           <b>Social Media for you and your mama.</b>
         </h1>
         <p>
-          Nestted is a social media platform that helps users stay in touch with
-          their families. Like a hilarious sitcom, users share touching moments
+          Nestted is a place for families of all types and sizes. Like a hilarious sitcom, families share touching moments
           with the world.
         </p>
         <button className="join-today">
